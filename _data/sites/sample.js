db34ea83072dff7a895584c0e7d07a8306f03625
Roger Knight <986557+AttractMore@users.noreply.github.com>
2020-12-28 18:23:38 +0100

    Add Blog

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,9 +2,10 @@ module.exports = {
 	name: "AttractMore", // optional, falls back to object key
 	description: "AttractMore",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 12, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.attractmore.uk"
+		"https://www.attractmore.uk",
+		"https://www.attractmore.uk/blog/"
 	]
 };
