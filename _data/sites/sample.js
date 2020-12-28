8e14afe7677c8ac7359445bbe2be2b3532aa3d06
Roger Knight <986557+AttractMore@users.noreply.github.com>
2020-12-28 18:25:39 +0100

    Add freshChrome:

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: "AttractMore", // optional, falls back to object key
 	description: "AttractMore",
 	options: {
-		frequency: 60 * 12, // (in minutes), 23 hours
+		frequency: 60 * 12, // (in minutes), 12 hours
+		freshChrome: "site"
 	},
 	urls: [
 		"https://www.attractmore.uk",
